b1a80e07769ec4ebb6dcc0f05318dca56bfa3c66
Rajat <rajat@kred.in>
2021-06-25 18:28:22 +0530

    return validStatus === true for all statuses

--- Networking.js
+++ Networking.js
@@ -6,52 +6,55 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NetworkFactory = exports.MyNetworkClient = void 0;
 const axios_1 = require("axios");
 class MyNetworkClient {
-    constructor() {
-        this.isReactive = false;
-        this.headers = {};
+  constructor() {
+    this.isReactive = false;
+    this.headers = {};
+  }
+  setHttpHeaders(headersMap) {
+    for (let key in headersMap) {
+      this.headers[key] = headersMap[key];
     }
-    setHttpHeaders(headersMap) {
-        for (let key in headersMap) {
-            this.headers[key] = headersMap[key];
-        }
-    }
-    _net() {
-        return axios_1.default.create({
-            timeout: 10000,
-            headers: {
-                ...this.headers
-            }
-        });
-    }
-    async getDataFromCache(url, params) {
-        return new Promise((resolve, reject) => {
-            this.networkGET(url, params).then(resolve, reject);
-        });
-    }
-    async networkGET(url, params) {
-        return this._net().get(url, { params });
-    }
-    async get(url, params) {
-        return this.getDataFromCache(url, params);
-    }
-    async post(url, params) {
-        return this._net().post(url, params);
-    }
-    async put(url, params) {
-        return this._net().put(url, params);
-    }
-    async delete(url, params) {
-        return this._net().put(url, params);
-    }
+  }
+  _net() {
+    return axios_1.default.create({
+      timeout: 10000,
+      validateStatus: function (status) {
+        return true; // default
+      },
+      headers: {
+        ...this.headers,
+      },
+    });
+  }
+  async getDataFromCache(url, params) {
+    return new Promise((resolve, reject) => {
+      this.networkGET(url, params).then(resolve, reject);
+    });
+  }
+  async networkGET(url, params) {
+    return this._net().get(url, { params });
+  }
+  async get(url, params) {
+    return this.getDataFromCache(url, params);
+  }
+  async post(url, params) {
+    return this._net().post(url, params);
+  }
+  async put(url, params) {
+    return this._net().put(url, params);
+  }
+  async delete(url, params) {
+    return this._net().put(url, params);
+  }
 }
 exports.MyNetworkClient = MyNetworkClient;
 class NetworkFactory {
-    static createSimpleClient() {
-        return NetworkFactory._createSimpleClient();
-    }
-    static _createSimpleClient(token) {
-        let net = new MyNetworkClient();
-        return net;
-    }
+  static createSimpleClient() {
+    return NetworkFactory._createSimpleClient();
+  }
+  static _createSimpleClient(token) {
+    let net = new MyNetworkClient();
+    return net;
+  }
 }
 exports.NetworkFactory = NetworkFactory;
--- Networking.ts
+++ Networking.ts
@@ -2,86 +2,64 @@
  * Created by rohittalwar on 13/04/16.
  */
 
-
-import Axios, * as axios from "axios"
-import { NetworkInterface  } from "./Protocols";
-
+import Axios, * as axios from "axios";
+import { NetworkInterface } from "./Protocols";
 
 export class MyNetworkClient implements NetworkInterface {
-    isReactive: boolean = false;
+  isReactive: boolean = false;
 
-    private headers: any = {}
-    constructor() {
+  private headers: any = {};
+  constructor() {}
+
+  setHttpHeaders(headersMap: any) {
+    for (let key in headersMap) {
+      this.headers[key] = headersMap[key];
     }
-  
-    setHttpHeaders(headersMap: any) {
-        for (let key in headersMap) {
-            this.headers[key] = headersMap[key]
-        }
-    }
-    private _net(): axios.AxiosInstance {
-        return Axios.create({
-            timeout: 10000,
-            headers: {
-                ...this.headers
-            }
-        })
-    }
-    
+  }
+  private _net(): axios.AxiosInstance {
+    return Axios.create({
+      timeout: 10000,
+      validateStatus: function (status) {
+        return true; // default
+      },
+      headers: {
+        ...this.headers,
+      },
+    });
+  }
 
-    
-    private async getDataFromCache(url: string, params?: Object): Promise<any> {
+  private async getDataFromCache(url: string, params?: Object): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.networkGET(url, params).then(resolve, reject);
+    });
+  }
+  private async networkGET(url: string, params?: Object): Promise<any> {
+    return this._net().get(url, { params });
+  }
+  async get(url: string, params?: Object): Promise<any> {
+    return this.getDataFromCache(url, params);
+  }
 
-        return new Promise((resolve, reject) => {
-            
-             this.networkGET(url, params).then(resolve, reject)
+  async post(url: string, params?: Object): Promise<any> {
+    return this._net().post(url, params);
+  }
 
+  async put(url: string, params?: Object): Promise<any> {
+    return this._net().put(url, params);
+  }
 
-        })
-
-    }
-    private async networkGET(url: string, params?: Object): Promise<any> {
-
-        return this._net().get(url, { params });
-
-    }
-    async get(url: string, params?: Object): Promise<any> {
-
-        return this.getDataFromCache(url, params)
-
-    }
-
-    async post(url: string, params?: Object): Promise<any> {
-
-        return this._net().post(url, params);
-
-
-    }
-
-    async put(url: string, params?: Object): Promise<any> {
-        return this._net().put(url, params);
-    }
-
-    async delete(url: string, params?: Object): Promise<any> {
-
-        return this._net().put(url, params);
-
-    }
-
-
+  async delete(url: string, params?: Object): Promise<any> {
+    return this._net().put(url, params);
+  }
 }
 
 export class NetworkFactory {
-  
-    static createSimpleClient(): NetworkInterface {
-        return NetworkFactory._createSimpleClient()
-    }
+  static createSimpleClient(): NetworkInterface {
+    return NetworkFactory._createSimpleClient();
+  }
 
-    private static _createSimpleClient(token?: string): NetworkInterface {
-        let net = new MyNetworkClient()
-        return net;
-    }
-
-    
-
+  private static _createSimpleClient(token?: string): NetworkInterface {
+    let net = new MyNetworkClient();
+    return net;
+  }
 }
